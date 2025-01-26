945224a0973910e8d79f6d61419e60c30d75c37e
Zuzky <sleepyzuzki@gmail.com>
2025-01-26 14:22:19 -0500

    Set up prod config

---
+++ config/env/production/server.ts
@@ -0,0 +1,9 @@
+// path: /config/env/production/server.ts
+
+export default ({ env }) => ({
+    proxy: true,
+    url: env('APP_URL'), // Sets the public URL of the application.
+    app: {
+        keys: env.array('APP_KEYS')
+    },
+});
